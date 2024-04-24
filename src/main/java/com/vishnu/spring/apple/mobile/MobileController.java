069c31b58ccb8104da1a8b00625a74d7059a5954
Vishnu M <vishnu2887.m@gmail.com>
2024-04-24 09:34:06 +0530

    Basic RestController forApple Mobile

--- src/main/java/com/vishnu/spring/apple/mobile/MobileController.java
+++ src/main/java/com/vishnu/spring/apple/mobile/MobileController.java
@@ -3,13 +3,15 @@ package com.vishnu.spring.apple.mobile;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "api/v1/mobile")
 public class MobileController {
 
     @GetMapping
     public List<String> getMobiles() {
-        return List.of("Apple", "Mobiles");
+        return List.of("Apple", "Mobiles", "Git", "Stage");
     }
 }
